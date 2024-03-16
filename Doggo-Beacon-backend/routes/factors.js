afd738e046476a98ffd1dd86df884dfe70b9f92d
rozan Karimi <rozan.karimii@gmail.com>
2024-03-16 13:01:51 -0400

    single id works

--- Doggo-Beacon-backend/routes/factors.js
+++ Doggo-Beacon-backend/routes/factors.js
@@ -2,8 +2,6 @@ const router = require("express").Router();
 const breedController = require("../controllers/breed_controller");
 
 router.get("/", breedController.index);
-router.get("/", breedController.show);
-// router.get("/", breedController.getDataFromDatabase);
-// router.get("/:id", breedController.getSingleRecord);
+router.get("/:id", breedController.show);
 
 module.exports = router;
