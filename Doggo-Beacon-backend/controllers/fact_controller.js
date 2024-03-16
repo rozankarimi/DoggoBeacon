f85b8ac05d7d7bb53b1775dbf031630c44feee48
rozan Karimi <rozan.karimii@gmail.com>
2024-03-16 13:05:30 -0400

    id added for dog facts

--- Doggo-Beacon-backend/controllers/fact_controller.js
+++ Doggo-Beacon-backend/controllers/fact_controller.js
@@ -8,7 +8,16 @@ async function index(req, res) {
     res.status(500).send(err.message);
   }
 }
+async function show(req, res) {
+  try {
+    const breeds = await Fact.find(req.params.id);
+    res.json(breeds);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+}
 
 module.exports = {
   index,
+  show,
 };
--- Doggo-Beacon-backend/routes/results.js
+++ Doggo-Beacon-backend/routes/results.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const factController = require("../controllers/fact_controller");
 
 router.get("/", factController.index);
+router.get("/:id", factController.show);
 
 module.exports = router;
