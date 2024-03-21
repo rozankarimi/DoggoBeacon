26eb3a8d4e35617d096e64137922e790a1e2bea7
rozan Karimi <rozan.karimii@gmail.com>
2024-03-21 09:48:27 -0400

    knex filtering applied

--- Doggo-Beacon-backend/controllers/comparison_controller.js
+++ Doggo-Beacon-backend/controllers/comparison_controller.js
@@ -8,127 +8,76 @@ const matchCriteria = async (req, res) => {
     // Get user input data from the request body
     const userInput = req.body;
     console.log(userInput);
-    // Fetch predefined criteria from the database
-    const criteria = await knex("dog_breed").select("*");
-
     const matchedCategories = [];
 
-    // Iterate over each condition
-    conditions.forEach(async (condition) => {
-      // Check if user selected "Apartment" or "House" and filter based on height criteria
-      if (
-        userInput.Category === "APARTMENT" &&
-        condition.height > 0 &&
-        condition.height < 20
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (userInput.Category === "HOUSE" && condition.height >= 20) {
-        matchedCategories.push(condition.category);
-      }
+    // Query the database with Knex
+    const criteria = await knex("dog_breed")
+      .select("*")
+      .where((builder) => {
+        // Filter based on user input
+        if (userInput.Category === "APARTMENT") {
+          builder.whereBetween("height", [0, 20]);
+        } else if (userInput.Category === "HOUSE") {
+          builder.where("height", ">=", 20);
+        }
 
-      // Check if user selected "Indoor" or "Short walk" or "long walk" and filter based on playfulness level
-      if (
-        userInput.PlayActivity === "ONLY INDOOR PLAY" &&
-        condition.playfulness === 3
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.PlayActivity === "SHORT WALK" &&
-        condition.playfulness === 4
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.PlayActivity === "OCCASIONAL LONG WALK" &&
-        condition.playfulness === 5
-      ) {
-        matchedCategories.push(condition.category);
-      }
+        if (userInput.PlayActivity === "ONLY INDOOR PLAY") {
+          builder.where("playfulness", 3);
+        } else if (userInput.PlayActivity === "SHORT WALK") {
+          builder.where("playfulness", 4);
+        } else if (userInput.PlayActivity === "OCCASIONAL LONG WALK") {
+          builder.where("playfulness", 5);
+        }
 
-      // Check if user selected "less than hour" or "4 Hours" or "8 Hours" and filter based on Energy level
+        if (userInput.AloneTime === "< 1 HOUR") {
+          builder.where("energy", 3);
+        } else if (userInput.AloneTime === "4 HOURS") {
+          builder.where("energy", 4);
+        } else if (userInput.AloneTime === " > 8 HOURS") {
+          builder.where("energy", 5);
+        }
 
-      if (userInput.EnergyLevel === "< 1 HOUR" && condition.energy === 3) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.EnergyLevel === "4 HOURS" &&
-        condition.energy === 4
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.EnergyLevel === "> 8 HOURS" &&
-        condition.energy === 5
-      ) {
-        matchedCategories.push(condition.category);
-      }
+        if (userInput.Shedding === "yes") {
+          builder.whereIn("shedding", [5, 4, 3, 2]);
+        } else {
+          builder.where("shedding", 1);
+        }
 
-      // Check user input for shedding
-      if (
-        userInput.Shedding === "YES" &&
-        [5, 4, 3, 2].includes(condition.shedding)
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (userInput.Shedding === "NO" && condition.shedding === 1) {
-        matchedCategories.push(condition.category);
-      }
-      // Check user input for trainability
-      if (userInput.Trainability === "NONE" && condition.trainability <= 3) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Trainability === "BASIC" &&
-        condition.trainability === 4
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Trainability === "ADVANCED" &&
-        condition.trainability === 5
-      ) {
-        matchedCategories.push(condition.category);
-      }
-      // Check user input for grooming
-      if (userInput.Grooming === "DAILY" && condition.grooming === 4) {
-        matchedCategories.push(condition.category);
-      } else if (userInput.Grooming === "WEEKLY" && condition.grooming === 3) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Grooming === "OCCASIONALLY" &&
-        [1, 2].includes(condition.grooming)
-      ) {
-        matchedCategories.push(condition.category);
-      }
-      // Check user input for weight
-      if (
-        userInput.Weight === "20 LBS OR UNDER" &&
-        condition.weight >= 0 &&
-        condition.weight <= 20
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Weight === "20-50 LBS" &&
-        condition.weight > 20 &&
-        condition.weight <= 50
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Weight === "50 LBS OR MORE" &&
-        condition.weight > 50
-      ) {
-        matchedCategories.push(condition.category);
-      }
+        if (userInput.Trainability === "NONE") {
+          builder.where("trainability", "<=", 3);
+        } else if (userInput.Trainability === "BASIC") {
+          builder.where("trainability", 4);
+        } else if (userInput.Trainability === "ADVANCED") {
+          builder.where("trainability", 5);
+        }
 
-      // Check user input for barking
-      if (userInput.Barking === "NONE" && condition.barking === 1) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Barking === "SOME BARKING IS OK" &&
-        [2, 3].includes(condition.barking)
-      ) {
-        matchedCategories.push(condition.category);
-      } else if (
-        userInput.Barking === "BARKING IS NOT AN ISSUE" &&
-        condition.barking === 5
-      ) {
-        matchedCategories.push(condition.category);
-      }
-    });
+        if (userInput.Grooming === "DAILY") {
+          builder.where("grooming", 4);
+        } else if (userInput.Grooming === "WEEKLY") {
+          builder.where("grooming", 3);
+        } else if (userInput.Grooming === "OCCASIONALLY") {
+          builder.whereIn("grooming", [1, 2]);
+        }
+
+        if (userInput.Weight === "20 LBS OR UNDER") {
+          builder.whereBetween("weight", [0, 20]);
+        } else if (userInput.Weight === "20-50 LBS") {
+          builder.whereBetween("weight", [20, 50]);
+        } else if (userInput.Weight === "50 LBS OR MORE") {
+          builder.where("weight", ">", 50);
+        }
+
+        if (userInput.Barking === "NONE") {
+          builder.where("barking", 1);
+        } else if (userInput.Barking === "SOME BARKING IS OK") {
+          builder.whereIn("barking", [2, 3]);
+        } else if (userInput.Barking === "BARKING IS NOT AN ISSUE") {
+          builder.where("barking", 5);
+        }
+      });
+
+    // Extract matched categories from the result
+    criteria.forEach((row) => matchedCategories.push(row.category));
 
     // Remove duplicate categories from matchedCategories if needed
     const uniqueMatchedCategories = [...new Set(matchedCategories)];
