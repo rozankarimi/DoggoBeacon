39e2ae1c946e6b36751a2f44bf1a19fb02a617ee
rozan Karimi <rozan.karimii@gmail.com>
2024-03-22 13:38:37 -0400

    broken questionnaire

--- Doggo-Beacon-backend/controllers/comparison_controller.js
+++ Doggo-Beacon-backend/controllers/comparison_controller.js
@@ -15,64 +15,88 @@ const matchCriteria = async (req, res) => {
       .select("*")
       .where((builder) => {
         // Filter based on user input
-        if (userInput.Category === "APARTMENT") {
+        if (userInput.answers === "APARTMENT") {
+          console.log("Applying category filter: APARTMENT");
           builder.whereBetween("height", [0, 20]);
-        } else if (userInput.Category === "HOUSE") {
+        } else if (userInput.answers === "HOUSE") {
+          console.log("Applying category filter: HOUSE");
           builder.where("height", ">=", 20);
         }
 
-        if (userInput.PlayActivity === "ONLY INDOOR PLAY") {
-          builder.where("playfulness", 3);
-        } else if (userInput.PlayActivity === "SHORT WALK") {
-          builder.where("playfulness", 4);
-        } else if (userInput.PlayActivity === "OCCASIONAL LONG WALK") {
-          builder.where("playfulness", 5);
+        if (userInput.answers === "ONLY INDOOR PLAY") {
+          console.log(
+            "make sure this is called when the user clicks only indoor play"
+          );
+          builder.andWhere("playfulness", 3);
+        } else if (userInput.answers === "SHORT WALK") {
+          console.log("Applying category filter: SHORT WALK");
+          builder.andWhere("playfulness", 4);
+        } else if (userInput.answers === "OCCASIONAL LONG WALK") {
+          console.log("Applying category filter: Occasional long walk");
+          builder.andWhere("playfulness", 5);
         }
 
-        if (userInput.AloneTime === "< 1 HOUR") {
-          builder.where("energy", 3);
-        } else if (userInput.AloneTime === "4 HOURS") {
-          builder.where("energy", 4);
-        } else if (userInput.AloneTime === " > 8 HOURS") {
-          builder.where("energy", 5);
+        if (userInput.answers === "< 1 HOUR") {
+          console.log("Applying category filter: 1 hour");
+          builder.andWhere("energy", 3);
+        } else if (userInput.answers === "4 HOURS") {
+          console.log("Applying category filter: 4 hours");
+          builder.andWhere("energy", 4);
+        } else if (userInput.answers === " > 8 HOURS") {
+          console.log("Applying category filter: 8 hours");
+          builder.andWhere("energy", 5);
         }
 
-        if (userInput.Shedding === "yes") {
-          builder.whereIn("shedding", [5, 4, 3, 2]);
+        if (userInput.answers === "yes") {
+          builder.andWhereIn("shedding", [5, 4, 3, 2]);
+          console.log("Applying category filter: yes");
         } else {
-          builder.where("shedding", 1);
+          console.log("Applying category filter: no sheeed");
+          builder.andWhere("shedding", 1);
         }
 
-        if (userInput.Trainability === "NONE") {
-          builder.where("trainability", "<=", 3);
-        } else if (userInput.Trainability === "BASIC") {
-          builder.where("trainability", 4);
-        } else if (userInput.Trainability === "ADVANCED") {
-          builder.where("trainability", 5);
+        if (userInput.answers === "NONE") {
+          console.log("Applying category filter: NONE");
+          builder.andWhere("trainability", "<=", 3);
+        } else if (userInput.answers === "BASIC") {
+          console.log("Applying category filter: BASIC");
+          builder.andWhere("trainability", 4);
+        } else if (userInput.answers === "ADVANCED") {
+          console.log("Applying category filter: ADVANCED");
+          builder.andWhere("trainability", 5);
         }
 
-        if (userInput.Grooming === "DAILY") {
-          builder.where("grooming", 4);
-        } else if (userInput.Grooming === "WEEKLY") {
-          builder.where("grooming", 3);
-        } else if (userInput.Grooming === "OCCASIONALLY") {
-          builder.whereIn("grooming", [1, 2]);
+        if (userInput.answers === "DAILY") {
+          console.log("Applying category filter: DAILY");
+          builder.andWhere("grooming", 4);
+        } else if (userInput.answers === "WEEKLY") {
+          console.log("Applying category filter: WEEKLY");
+          builder.andWhere("grooming", 3);
+        } else if (userInput.answers === "OCCASIONALLY") {
+          console.log("Applying category filter: OCCASIONALLY");
+          builder.andWhereIn("grooming", [1, 2]);
         }
 
-        if (userInput.Weight === "20 LBS OR UNDER") {
+        if (userInput.answers === "20 LBS OR UNDER") {
+          console.log("Applying category filter:  < 20");
           builder.whereBetween("weight", [0, 20]);
-        } else if (userInput.Weight === "20-50 LBS") {
+        } else if (userInput.answers === "20-50 LBS") {
+          console.log("Applying category filter: 20-50");
           builder.whereBetween("weight", [20, 50]);
-        } else if (userInput.Weight === "50 LBS OR MORE") {
+        } else if (userInput.answers === "50 LBS OR MORE") {
+          console.log("Applying category filter: > 50");
           builder.where("weight", ">", 50);
         }
 
-        if (userInput.Barking === "NONE") {
-          builder.where("barking", 1);
-        } else if (userInput.Barking === "SOME BARKING IS OK") {
-          builder.whereIn("barking", [2, 3]);
-        } else if (userInput.Barking === "BARKING IS NOT AN ISSUE") {
-          builder.where("barking", 5);
+        if (userInput.answers === "NONE") {
+          console.log("Applying category filter: NONE");
+          builder.andWhere("barking", 1);
+        } else if (userInput.answers === "SOME BARKING IS OK") {
+          console.log("Applying category filter: Some is ok");
+          builder.andWhereIn("barking", [2, 3]);
+        } else if (userInput.answers === "BARKING IS NOT AN ISSUE") {
+          console.log("Applying category filter: Not an issue");
+          builder.andWhere("barking", 5);
         }
       });
 
