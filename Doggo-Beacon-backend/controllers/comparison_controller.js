072c31647804c7d1801e575753d4ac2081edf3d9
rozan Karimi <rozan.karimii@gmail.com>
2024-03-24 00:32:03 -0400

    backend logic works

--- Doggo-Beacon-backend/controllers/comparison_controller.js
+++ Doggo-Beacon-backend/controllers/comparison_controller.js
@@ -2,115 +2,325 @@
 const router = require("../routes/comparison");
 const knex = require("knex")(require("../knexfile").development);
 
+const matchedCategories = [];
+
 // Controller action to handle matching logic
 const matchCriteria = async (req, res) => {
   try {
     // Get user input data from the request body
     const userInput = req.body;
     console.log(userInput);
-    const matchedCategories = [];
+    matchedCategories.length = 0;
 
     // Query the database with Knex
-    const criteria = await knex("dog_breed")
-      .select("*")
-      .where((builder) => {
-        // Filter based on user input
-        if (userInput.height === "APARTMENT") {
-          console.log("Applying category filter: APARTMENT");
-          builder.whereBetween("height", [0, 20]);
-        } else if (userInput.height === "HOUSE") {
-          console.log("Applying category filter: HOUSE");
-          builder.where("height", ">=", 20);
-        }
+    let heightFac;
+    if (userInput.height === "APARTMENT") {
+      console.log("Applying category filter: APARTMENT");
+      heightFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .whereBetween("height", [0, 20]);
+    } else if (userInput.height === "HOUSE") {
+      console.log("Applying category filter: HOUSE");
+      heightFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .where("height", ">=", 20);
+    }
+    let playFac;
+    if (userInput.playfulness === "ONLY INDOOR PLAY") {
+      console.log("Applying category filter: only indoor play");
+      playFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("playfulness", 3);
+      let filteredArray = heightFac.filter(function (array_el) {
+        return (
+          playFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.playfulness === "SHORT WALK") {
+      console.log("Applying category filter: SHORT WALK");
+      playFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("playfulness", 4);
+      let filteredArray = heightFac.filter(function (array_el) {
+        return (
+          playFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.playfulness === "OCCASIONAL LONG WALK") {
+      console.log("Applying category filter: Occasional long walk");
+      playFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("playfulness", 5);
+      let filteredArray = heightFac.filter(function (array_el) {
+        return (
+          playFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    }
+    let energyFac;
+    if (userInput.energy === " < 1 HOUR") {
+      console.log("Applying category filter: 1 hour");
+      energyFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("energy", 3);
+      let filteredArray2 = playFac.filter(function (array_el) {
+        return (
+          energyFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+      // console.log(filteredArray2);
+    } else if (userInput.energy === "4 HOURS") {
+      console.log("Applying category filter: 4 hours");
+      energyFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("energy", 4);
+      let filteredArray2 = playFac.filter(function (array_el) {
+        return (
+          energyFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.energy === " > 8 HOURS") {
+      console.log("Applying category filter: 8 hours");
+      energyFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("energy", 5);
+      let filteredArray2 = playFac.filter(function (array_el) {
+        return (
+          energyFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    }
 
-        if (userInput.playfulness === "ONLY INDOOR PLAY") {
-          console.log("Applying category filter: only indoor play");
-          builder.andWhere("playfulness", 3);
-        } else if (userInput.playfulness === "SHORT WALK") {
-          console.log("Applying category filter: SHORT WALK");
-          builder.andWhere("playfulness", 4);
-        } else if (userInput.playfulness === "OCCASIONAL LONG WALK") {
-          console.log("Applying category filter: Occasional long walk");
-          builder.andWhere("playfulness", 5);
-        }
+    let shedFac;
+    if (userInput.shedding === "YES") {
+      shedFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .whereIn("shedding", [5, 4, 3, 2]);
+      let filteredArray3 = energyFac.filter(function (array_el) {
+        return (
+          shedFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+      console.log("Applying category filter: yes");
+    } else if (userInput.shedding === "NO") {
+      console.log("Applying category filter: no sheeed");
+      shedFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("shedding", 1);
+      let filteredArray3 = energyFac.filter(function (array_el) {
+        return (
+          shedFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    }
 
-        if (userInput.energy === " < 1 HOUR") {
-          console.log("Applying category filter: 1 hour");
-          builder.andWhere("energy", 3);
-        } else if (userInput.energy === "4 HOURS") {
-          console.log("Applying category filter: 4 hours");
-          builder.andWhere("energy", 4);
-        } else if (userInput.energy === " > 8 HOURS") {
-          console.log("Applying category filter: 8 hours");
-          builder.andWhere("energy", 5);
-        }
+    let trainFac;
+    if (userInput.training === "NONE") {
+      console.log("Applying category filter: NONE");
+      trainFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("trainability", "<=", 3);
+      let filteredArray4 = shedFac.filter(function (array_el) {
+        return (
+          trainFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.training === "BASIC") {
+      console.log("Applying category filter: BASIC");
+      trainFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("trainability", 4);
+      let filteredArray4 = shedFac.filter(function (array_el) {
+        return (
+          trainFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.training === "ADVANCED") {
+      console.log("Applying category filter: ADVANCED");
+      trainFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("trainability", 5);
+      let filteredArray4 = shedFac.filter(function (array_el) {
+        return (
+          trainFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+      // console.log(filteredArray4);
+    }
 
-        if (userInput.shedding === "YES") {
-          builder.whereIn("shedding", [5, 4, 3, 2]);
-          console.log("Applying category filter: yes");
-        } else if (userInput.shedding === "NO") {
-          console.log("Applying category filter: no sheeed");
-          builder.andWhere("shedding", 1);
-        }
+    let groomFac;
+    if (userInput.grooming === "DAILY") {
+      console.log("Applying category filter: DAILY");
+      groomFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("grooming", 4);
+      let filteredArray5 = trainFac.filter(function (array_el) {
+        return (
+          groomFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.grooming === "WEEKLY") {
+      console.log("Applying category filter: WEEKLY");
+      groomFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .andWhere("grooming", 3);
+      let filteredArray5 = trainFac.filter(function (array_el) {
+        return (
+          groomFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.grooming === "OCCASIONALLY") {
+      console.log("Applying category filter: OCCASIONALLY");
+      groomFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .whereIn("grooming", [1, 2]);
+      let filteredArray5 = trainFac.filter(function (array_el) {
+        return (
+          groomFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    }
 
-        if (userInput.training === "NONE") {
-          console.log("Applying category filter: NONE");
-          builder.andWhere("trainability", "<=", 3);
-        } else if (userInput.training === "BASIC") {
-          console.log("Applying category filter: BASIC");
-          builder.andWhere("trainability", 4);
-        } else if (userInput.training === "ADVANCED") {
-          console.log("Applying category filter: ADVANCED");
-          builder.andWhere("trainability", 5);
-        }
+    let weightFac;
+    if (userInput.weight === "20 LBS OR UNDER  ") {
+      console.log("Applying category filter:  < 20");
+      weightFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .whereBetween("weight", [0, 20]);
+      let filteredArray6 = groomFac.filter(function (array_el) {
+        return (
+          weightFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.weight === "20-50 LBS ") {
+      console.log("Applying category filter: 20-50");
+      weightFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .whereBetween("weight", [20, 50]);
+      let filteredArray6 = groomFac.filter(function (array_el) {
+        return (
+          weightFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    } else if (userInput.weight === "50 LBS OR MORE") {
+      console.log("Applying category filter: > 50");
+      weightFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .where("weight", ">", 50);
+      let filteredArray6 = groomFac.filter(function (array_el) {
+        return (
+          weightFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+    }
 
-        if (userInput.grooming === "DAILY") {
-          console.log("Applying category filter: DAILY");
-          builder.andWhere("grooming", 4);
-        } else if (userInput.grooming === "WEEKLY") {
-          console.log("Applying category filter: WEEKLY");
-          builder.andWhere("grooming", 3);
-        } else if (userInput.grooming === "OCCASIONALLY") {
-          console.log("Applying category filter: OCCASIONALLY");
-          builder.whereIn("grooming", [1, 2]);
-        }
-
-        if (userInput.weight === "20 LBS OR UNDER  ") {
-          console.log("Applying category filter:  < 20");
-          builder.whereBetween("weight", [0, 20]);
-        } else if (userInput.weight === "20-50 LBS ") {
-          console.log("Applying category filter: 20-50");
-          builder.whereBetween("weight", [20, 50]);
-        } else if (userInput.weight === "50 LBS OR MORE") {
-          console.log("Applying category filter: > 50");
-          builder.where("weight", ">", 50);
-        }
-
-        if (userInput.bark === "NONE") {
-          console.log("Applying category filter: NONE");
-          builder.andWhere("barking", 1);
-        } else if (userInput.bark === "SOME BARKING IS OK ") {
-          console.log("Applying category filter: Some is ok");
-          builder.whereIn("barking", [2, 3]);
-        } else if (userInput.bark === "BARKING IS NOT AN ISSUE  ") {
-          console.log("Applying category filter: Not an issue");
-          builder.andWhere("barking", 5);
-          debugger;
-        }
+    let barkFac;
+    if (userInput.bark === "NONE") {
+      console.log("Applying category filter: NONE");
+      barkFac = await knex.select("*").from("dog_breed").andWhere("barking", 1);
+      let filteredArray7 = weightFac.filter(function (array_el) {
+        return (
+          barkFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
       });
 
-    // Extract matched categories from the result
-    criteria.forEach((row) => {
-      console.log(row);
+      matchedCategories.push(...filteredArray7);
+    } else if (userInput.bark === "SOME BARKING IS OK ") {
+      console.log("Applying category filter: Some is ok");
+      barkFac = await knex
+        .select("*")
+        .from("dog_breed")
+        .whereIn("barking", [2, 3]);
+      let filteredArray7 = weightFac.filter(function (array_el) {
+        return (
+          barkFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+
+      matchedCategories.push(...filteredArray7);
       console.log(matchedCategories);
-      matchedCategories.push({ name: row.name, image: row.image_link });
-    });
+    } else if (userInput.bark === "BARKING IS NOT AN ISSUE  ") {
+      console.log("Applying category filter: Not an issue");
+      barkFac = await knex.select("*").from("dog_breed").andWhere("barking", 5);
+      let filteredArray7 = weightFac.filter(function (array_el) {
+        return (
+          barkFac.filter(function (anotherOne_el) {
+            return anotherOne_el.id == array_el.id;
+          }).length == 0
+        );
+      });
+      matchedCategories.push(...filteredArray7);
+      console.log(matchedCategories);
+    }
+
+    const extractedData = matchedCategories.map(({ name, image_link }) => ({
+      name,
+      image_link,
+    }));
+    console.log(extractedData);
 
     // Remove duplicate categories from matchedCategories if needed
     const uniqueMatchedCategories = [...new Set(matchedCategories)];
 
     // Send back the matched categories as the response
-    res.json({ matchedCategories });
+    res.json({ matchedCategories: extractedData });
   } catch (error) {
     console.error("Error matching criteria:", error);
     res.status(500).json({ error: "Internal server error" });
