b3d61a3ea99d0b4df13690f60f0ad639f2b1f2b7
rozan Karimi <rozan.karimii@gmail.com>
2024-03-24 21:19:18 -0400

    comments removed

--- Doggo-Beacon-backend/models/compare.js
+++ Doggo-Beacon-backend/models/compare.js
@@ -1,14 +1,1 @@
 const Result = require("./result");
-
-// async function matchCriteria(req, res) {
-//   try {
-//     const breeds = await Breed.all();
-//     res.status(200).json(breeds);
-//   } catch (err) {
-//     res.status(500).send(err.message);
-//   }
-// }
-
-// module.exports = {
-//   matchCriteria,
-// };
